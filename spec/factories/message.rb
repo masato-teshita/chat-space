7732f8d38706a25a9a1579f3ee6abf6cf9de1fc5
m.teshita <masato.t2207@gmail.com>
2020-02-24 15:12:18 +0900

    Summary

---
+++ spec/factories/message.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  
+  factory :message do
+    content {Faker::Lorem.sentence}
+    image   { File.open("#{Rails.root}/public/images/sample1.jpeg")}
+    user
+    group
+  end
+
+end
