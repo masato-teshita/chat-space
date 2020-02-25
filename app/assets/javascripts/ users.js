681c83fdc2a54b7a5ffe1b2c74f40c0af3e634b7
m.teshita <masato.t2207@gmail.com>
2020-02-25 21:00:45 +0900

    Add function of appending html

--- app/assets/javascripts/ users.js
+++ app/assets/javascripts/ users.js
@@ -1,4 +1,24 @@
 $(function() {
+  var search_result = $('#user-search-result');
+
+  function addUser(user) {
+    var html = `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `
+    search_result.append(html);
+  }
+
+  function addErrMsgHTML(msg) {
+    var html = `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>
+               `
+    search_result.append(html)
+  }
 
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
@@ -10,10 +30,19 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users){
-      console.log("成功です")
+      search_result.empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          addUser(user);
+        });
+      } else if (input == "") {
+        return false;
+      } else {
+        addErrMsgHTML("ユーザーが見つかりません");
+      }
     })
     .fail(function(){
-      console.log("失敗です")
+      alert("ユーザー検索に失敗しました");
     })
   });
 });
