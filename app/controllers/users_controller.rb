33663edea8260a1946b512020862cea2ebd730c6
m.teshita <masato.t2207@gmail.com>
2020-02-25 19:29:04 +0900

    Setting API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    # @users = User.find(params[:name])
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
